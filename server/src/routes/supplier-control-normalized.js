64e4f6714cf409f03d4719de6f855b10c21c0f32
xrhstosmanis-design <xrhstosmanis@gmail.com>
2026-08-10 00:29:36 +0300

    Mount supplier product catalog route

--- server/src/routes/supplier-control-normalized.js
+++ server/src/routes/supplier-control-normalized.js
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import supplierGlobalReportRoutes from "./supplier-global-reports.js";
+import supplierProductCatalogRoutes from "./supplier-product-catalog.js";
 import supplierProductTransferRoutes from "./supplier-product-transfer.js";
 import supplierControlRoutes from "./supplier-control.js";
 import {ensureSupplierControlCompatibility} from "../supplier-control-bootstrap.js";
@@ -22,6 +23,7 @@ router.use((req,res,next)=>{
   next();
 });
 router.use(supplierGlobalReportRoutes);
+router.use(supplierProductCatalogRoutes);
 router.use(supplierProductTransferRoutes);
 router.use(supplierControlRoutes);
 export default router;
