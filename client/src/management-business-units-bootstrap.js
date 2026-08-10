80136395841bef9ccae5f9ceb98611ea4de4f248
xrhstosmanis-design <xrhstosmanis@gmail.com>
2026-08-11 00:59:38 +0300

    Bootstrap banks and shipping methods management

---
+++ client/src/management-business-units-bootstrap.js
@@ -0,0 +1,7 @@
+import {installManagementBusinessUnitsSuite} from "./components/commerce/installManagementBusinessUnitsSuite.js";
+const api=async(path,options={})=>{const token=localStorage.getItem("token");const response=await fetch(path,{...options,headers:{"Content-Type":"application/json",...(token?{Authorization:`Bearer ${token}`}:{}) ,...(options.headers||{})}});const data=await response.json().catch(()=>({}));if(!response.ok)throw new Error(data.error||`Σφάλμα ${response.status}`);return data};
+const install=()=>installManagementBusinessUnitsSuite(api);
+const schedule=()=>[0,60,180,400,800].forEach(ms=>window.setTimeout(install,ms));
+schedule();
+document.addEventListener("click",schedule,true);
+window.addEventListener("myworkstation:modules-updated",schedule);
