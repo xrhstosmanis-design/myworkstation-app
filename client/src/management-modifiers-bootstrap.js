5d5970b0c4b21112daf4676b90f3f2abaab21942
xrhstosmanis-design <xrhstosmanis@gmail.com>
2026-08-11 00:29:56 +0300

    Bootstrap management modifiers suite

---
+++ client/src/management-modifiers-bootstrap.js
@@ -0,0 +1,8 @@
+import {installManagementModifiersSuite} from "./components/commerce/installManagementModifiersSuite.js";
+
+const api=async(path,options={})=>{const token=localStorage.getItem("token");const response=await fetch(path,{...options,headers:{"Content-Type":"application/json",...(token?{Authorization:`Bearer ${token}`}:{}) ,...(options.headers||{})}});const data=await response.json().catch(()=>({}));if(!response.ok)throw new Error(data.error||`Σφάλμα ${response.status}`);return data};
+const install=()=>installManagementModifiersSuite(api);
+const schedule=()=>[0,60,180,400,800].forEach(ms=>window.setTimeout(install,ms));
+schedule();
+document.addEventListener("click",schedule,true);
+window.addEventListener("myworkstation:modules-updated",schedule);
