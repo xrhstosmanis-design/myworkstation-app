eb2f3132bbabe238b21751151fa3d33dbba7da4a
xrhstosmanis-design <xrhstosmanis@gmail.com>
2026-08-23 02:09:08 +0300

    feat(invoice-learning): load pending barcode registry

--- client/src/entry-dispatch.js
+++ client/src/entry-dispatch.js
@@ -4,6 +4,7 @@ if(path==='/platform-admin/invoice-learning-lab'){
   import('./invoice-learning-server-sync-bootstrap.js')
     .then(()=>import('./invoice-learning-lab-bootstrap.js'))
     .then(()=>import('./invoice-learning-taxonomy-bootstrap.js'))
+    .then(()=>import('./invoice-learning-pending-barcodes-bootstrap.js'))
     .catch(error=>{
       console.error('Invoice Learning Lab bootstrap failed.',error);
     });
