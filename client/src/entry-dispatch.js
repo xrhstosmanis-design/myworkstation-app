51ee7587668a4f42bd452a3c61592da335ea7c5a
xrhstosmanis-design <xrhstosmanis@gmail.com>
2026-08-24 20:01:03 +0300

    feat(invoice-learning): load Azure-only invoice reader

--- client/src/entry-dispatch.js
+++ client/src/entry-dispatch.js
@@ -3,6 +3,7 @@ const path=window.location.pathname.replace(/\/+$/,'');
 if(path==='/platform-admin/invoice-learning-lab'){
   import('./invoice-learning-server-sync-bootstrap.js')
     .then(()=>import('./invoice-learning-lab-bootstrap.js'))
+    .then(()=>import('./invoice-learning-azure-only-reader.js'))
     .then(()=>import('./invoice-learning-taxonomy-bootstrap.js'))
     .then(()=>import('./invoice-learning-pending-barcodes-bootstrap.js'))
     .then(()=>import('./invoice-learning-generate-barcode-extension.js'))
