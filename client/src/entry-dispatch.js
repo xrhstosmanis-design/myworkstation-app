7069d8ef4fdb099bdebc99e5a7f0f2755488a5ba
xrhstosmanis-design <xrhstosmanis@gmail.com>
2026-08-24 16:18:55 +0300

    feat(invoice-learning): load product form select controls

--- client/src/entry-dispatch.js
+++ client/src/entry-dispatch.js
@@ -6,6 +6,7 @@ if(path==='/platform-admin/invoice-learning-lab'){
     .then(()=>import('./invoice-learning-taxonomy-bootstrap.js'))
     .then(()=>import('./invoice-learning-pending-barcodes-bootstrap.js'))
     .then(()=>import('./invoice-learning-generate-barcode-extension.js'))
+    .then(()=>import('./invoice-learning-product-form-selects-extension.js'))
     .catch(error=>{
       console.error('Invoice Learning Lab bootstrap failed.',error);
     });
