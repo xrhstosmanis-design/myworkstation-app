77f80bca5d0399ed611b74b87bc1d418cc7a27f2
xrhstosmanis-design <xrhstosmanis@gmail.com>
2026-08-27 00:27:57 +0300

    Run screen recording in a detached window
    * Keep screen recording in a detached window
    
    * Keep screen recording in a detached window
    
    * Keep screen recording in a detached window
    
    * Keep screen recording in a detached window
    
    * Keep screen recording in a detached window
    
    * Keep screen recording in a detached window

--- client/src/entry-dispatch.js
+++ client/src/entry-dispatch.js
@@ -1,6 +1,8 @@
 const path=window.location.pathname.replace(/\/+$/,'');
 
-if(path==='/platform-admin/invoice-learning-lab'){
+if(path==='/screen-recorder'){
+  import('./screen-recorder-entry.jsx');
+}else if(path==='/platform-admin/invoice-learning-lab'){
   import('./invoice-learning-server-sync-bootstrap.js')
     .then(()=>import('./invoice-learning-lab-bootstrap.js'))
     .then(()=>import('./invoice-learning-azure-only-reader.js'))
