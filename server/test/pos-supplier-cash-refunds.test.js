916755276377b73128add7d17ddd29a8e8c14cbe
xrhstosmanis-design <xrhstosmanis@gmail.com>
2026-08-30 00:48:48 +0300

    test(pos): cover generic negative other expense flow

--- server/test/pos-supplier-cash-refunds.test.js
+++ server/test/pos-supplier-cash-refunds.test.js
@@ -5,13 +5,13 @@ import {readFile} from "node:fs/promises";
 const ui=await readFile(new URL("../../client/src/components/store/StorePosPaymentsModal.jsx",import.meta.url),"utf8");
 const route=await readFile(new URL("../src/routes/store-transactions.js",import.meta.url),"utf8");
 
-test("supplier cash refund is a negative payment that returns money to shift cash",()=>{
-  assert.match(ui,/Επιστροφή χρημάτων από προμηθευτή/);
+test("negative other expense is a negative payment that returns money to shift cash",()=>{
+  assert.match(ui,/Επιστροφή χρημάτων \/ αρνητικό έξοδο/);
   assert.match(ui,/Αρνητική πληρωμή/);
   assert.match(ui,/amount:supplierRefund\?-amount:amount/);
   assert.match(ui,/paymentSource="CASH_SHIFT"/);
-  assert.match(ui,/Προμηθευτής/);
+  assert.match(ui,/Δεν αφορά προμηθευτή/);
   assert.match(route,/body\.amount<0/);
-  assert.match(route,/SUPPLIER_PAYMENT/);
+  assert.match(route,/OTHER_EXPENSE/);
   assert.match(route,/CASH_SHIFT/);
 });
