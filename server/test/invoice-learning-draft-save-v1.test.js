354b4b1f255e627b6634f001f6430981af011d29
xrhstosmanis-design <xrhstosmanis@gmail.com>
2026-09-21 21:28:02 +0300

    Fix draft-save regression test escaping

--- server/test/invoice-learning-draft-save-v1.test.js
+++ server/test/invoice-learning-draft-save-v1.test.js
@@ -5,8 +5,8 @@ import fs from "node:fs/promises";
 test("Invoice Learning draft save persists the workspace without waiting for profile sync",async()=>{
   const client=await fs.readFile(new URL("../../client/src/invoice-learning-lab-bootstrap.js",import.meta.url),"utf8");
   const route=await fs.readFile(new URL("../src/routes/platform-invoice-learning-workspace.js",import.meta.url),"utf8");
-  assert.match(client,/persistWorkspaceNow\\(\\{syncProfiles:false\\}\\)/);
-  assert.match(client,/AbortSignal\\.timeout\\(30000\\)/);
-  assert.match(route,/const syncProfiles=req\\.body\\?\\.syncProfiles!==false/);
-  assert.match(route,/if\\(syncProfiles\\)await upsertSupplierProfiles/);
+  assert.match(client,/persistWorkspaceNow\(\{syncProfiles:false\}\)/);
+  assert.match(client,/AbortSignal\.timeout\(30000\)/);
+  assert.match(route,/const syncProfiles=req\.body\?\.syncProfiles!==false/);
+  assert.match(route,/if\(syncProfiles\)await upsertSupplierProfiles/);
 });
